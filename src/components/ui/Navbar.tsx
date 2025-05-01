a734eb7823744f6966dbd04529800a16c14d4899
MirzaJalal <mirzajalal25@email.com>
2025-05-01 11:29:28 +0200

    colorModeButton

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,11 +1,15 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../../assets/logo.webp";
+import { ColorModeButton } from "@/components/ui/color-mode";
 
 const Navbar = () => {
   return (
     <HStack>
       <Image src={logo} alt="Logo" boxSize="50px" />
-      <Text>Navbar</Text>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <Text>Navbar</Text>
+        <ColorModeButton />
+      </div>
     </HStack>
   );
 };
